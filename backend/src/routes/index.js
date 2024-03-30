2bc894df5ac224f26edf17e079176a5672c6ab22
LucasLee0327 <0327lucas@gmail.com>
2024-03-31 00:30:11 +0800

    init

---
+++ backend/src/routes/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {glob} from "glob";
+import users from "./api/v1/users/index.js";
+//import csrf from "./api/v1/csrf-token";
+
+const rootRouter = express.Router();
+
+rootRouter.use("/api/v1/users", users)
+//rootRouter.use("/api/v1/csrf-token", )
+
+export default rootRouter;
+
+//async function autoloadRoutes() {
+//  const jsfiles = await glob("**/index.js", {
+//    cwd: "src/routes",
+//    ignore: "index.js",
+//  });
+//  const importTasks = jsfiles.map(async (path) => {
+//    const module = await import(`./${path}`);
+//    if (module.default === undefined) return;
+//    rootRouter.use(`/${path.slice(0, -9)}`, module.default);
+//  });
+//  return Promise.all(importTasks);
+//}
+//await autoloadRoutes();
